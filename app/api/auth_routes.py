019e2d639ac4d836f884809361f22f1c237e8a2e
Alexandra <ale.vallem@gmail.com>
2023-08-09 01:01:26 -0400

    missing AWS for sign up

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -3,6 +3,7 @@ from app.models import User, db
 from app.forms import LoginForm
 from app.forms import SignUpForm
 from flask_login import current_user, login_user, logout_user, login_required
+from .AWS_helpers import upload_file_to_s3, get_unique_filename
 
 auth_routes = Blueprint('auth', __name__)
 
@@ -62,9 +63,15 @@ def sign_up():
     form = SignUpForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
+
+        image_file = form.data['profile_img']
+        image_file.filename = get_unique_filename(image_file.filename)
+        upload = upload_file_to_s3(image_file)
+
         user = User(
             email=form.data['email'],
             password=form.data['password'],
+            profile_img=upload['url'],
             first_name=form.data['first_name'],
             last_name=form.data['last_name'],
             birth_date=form.data['birth_date'],
--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,5 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, DateField, SelectField, TextAreaField
+from wtforms import StringField, PasswordField, DateField, SelectField, TextAreaField, FileField
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from ..api.AWS_helpers import ALLOWED_EXTENSIONS
 from wtforms.validators import DataRequired, ValidationError, EqualTo, Length
 from app.models import User
 from enum import Enum
@@ -31,6 +33,7 @@ class SignUpForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), user_exists, is_valid_email ])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    profile_img = FileField('Profile Image', validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
     birth_date = DateField('Birth Date', validators=[DataRequired()])
