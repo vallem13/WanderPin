9d03ad066f29a2c2503cf2844531b1562db6a367
Alexandra <ale.vallem@gmail.com>
2023-08-09 22:25:06 -0400

    get all pins and get single pin routes

---
+++ app/api/__init__.py
@@ -0,0 +1,3 @@
+from user_routes import user_routes
+from auth_routes import auth_routes
+from pin_routes import pin_routes
--- app/api/pin_routes.py
+++ app/api/pin_routes.py
@@ -1,6 +1,24 @@
 from flask import Blueprint, jsonify
-from flask_login import login_required
-from app.models import Pin
+from flask_login import current_user, login_required
+from .aws_helpers import (upload_file_to_s3, get_unique_filename)
+from app.models import Pin, db
 
 
-user_routes = Blueprint('pins', __name__)
+pin_routes = Blueprint('pins', __name__)
+
+# Get all Pins
+@pin_routes.route('/')
+@login_required
+def getAllPins():
+
+    pins = Pin.query.all()
+    return [pin.to_dict() for pin in pins]
+
+
+# Get Pin by ID
+@pin_routes.route('/<int:pinId>')
+@login_required
+def get_restaurant(pinId):
+
+    pin = Pin.query.get(pinId)
+    return pin.to_dict()
