193cc848a735cd1ffa1379b935d1fdffd8bbf7cc
Alexandra <ale.vallem@gmail.com>
2023-08-11 02:52:33 -0400

    seeds for voards and pin-boards

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,8 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .pins import seed_pins, undo_pins
+from .boards import seed_boards, undo_boards
+from .pins_boards import seed_pins_boards, undo_pins_boards
 
 from app.models.db import db, environment, SCHEMA
 
@@ -13,11 +15,15 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
+        undo_pins_boards()
+        undo_boards()
         undo_pins()
         undo_users()
 
     seed_users()
     seed_pins()
+    seed_boards()
+    seed_pins_boards()
 
     print('------> db seeded')
 
@@ -25,6 +31,8 @@ def seed():
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_pins_boards()
+    undo_boards()
     undo_pins()
     undo_users()
 
