945f3df68045aae89eb20c04d328135bf30a482b
Alexandra <ale.vallem@gmail.com>
2023-08-13 17:57:07 -0400

    display boards without images

--- react-app/src/components/UserProfilePage/BoardCard.js
+++ react-app/src/components/UserProfilePage/BoardCard.js
@@ -12,9 +12,9 @@ const BoardCard = ({ board }) => {
     return (
         <div onClick={onClick}>
             <div>
-                <img src={board.pinImgs[0]} alt={board.title} style={{ width: '100px', height: '100px' }} />
-                <img src={board.pinImgs[1]} alt={board.title} style={{ width: '100px', height: '100px' }} />
-                <img src={board.pinImgs[2]} alt={board.title} style={{ width: '100px', height: '100px' }} />
+                <img src={board.pinImgs[0] ? board.pinImgs[0] : 'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg'} alt={board.title} style={{ width: '100px', height: '100px' }} />
+                <img src={board.pinImgs[1] ? board.pinImgs[1] : 'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg'} alt={board.title} style={{ width: '50px', height: '50px' }} />
+                <img src={board.pinImgs[2] ? board.pinImgs[2] : 'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg'} alt={board.title} style={{ width: '50px', height: '50px' }} />
             </div>
         </div>
     )
