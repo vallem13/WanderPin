b20ec15c6b8383813cd4818174c65302615a80e1
Alexandra <ale.vallem@gmail.com>
2023-08-16 00:37:37 -0400

    prifile css

--- react-app/src/components/UserProfilePage/BoardCard.js
+++ react-app/src/components/UserProfilePage/BoardCard.js
@@ -10,11 +10,13 @@ const BoardCard = ({ board }) => {
     }
 
     return (
-        <div onClick={onClick}>
-            <div>
-                <img src={board.pinImgs[0] ? board.pinImgs[0] : 'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg'} alt={board.title} style={{ width: '100px', height: '100px' }} />
-                <img src={board.pinImgs[1] ? board.pinImgs[1] : 'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg'} alt={board.title} style={{ width: '50px', height: '50px' }} />
-                <img src={board.pinImgs[2] ? board.pinImgs[2] : 'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg'} alt={board.title} style={{ width: '50px', height: '50px' }} />
+        <div className='boardCard' onClick={onClick}>
+            <div className='boardCard-prevImg'>
+                <img src={board.pinImgs[0] ? board.pinImgs[0] : 'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg'} alt={board.title} className='prevImg' />
+            </div>
+            <div className='boardCard-images'>
+                <img src={board.pinImgs[1] ? board.pinImgs[1] : 'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg'} alt={board.title} className='smallImg' />
+                <img src={board.pinImgs[2] ? board.pinImgs[2] : 'https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg'} alt={board.title} className='smallImg' />
             </div>
         </div>
     )
--- react-app/src/components/UserProfilePage/index.js
+++ react-app/src/components/UserProfilePage/index.js
@@ -5,13 +5,9 @@ import { getAllBoardsThunk } from "../../store/board";
 import { getAllPinsThunk } from "../../store/pin";
 import BoardCard from "./BoardCard";
 import PinCard from "../HomePage/PinCard";
-import RemovePinBoard from "../AddRemovePinBoard/RemovePinBoard";
-import OpenModalButton from '../OpenModalButton'
 import './UserProfile.css';
 
-
 const UserProfile = () => {
-
     const dispatch = useDispatch();
     const user = useSelector(state => state.session.user)
     const pinsObj = useSelector(state => state.pins.allPins)
@@ -21,25 +17,52 @@ const UserProfile = () => {
     const boards = boardsObj ? Object.values(boardsObj) : [];
     const user_boards = boards.filter(boards => boards.user_id === user.id)
 
+    const [displayType, setDisplayType] = useState("pin"); // Default display type is "pin"
+
     useEffect(() => {
         dispatch(getAllBoardsThunk());
         dispatch(getAllPinsThunk());
     }, [dispatch]);
 
+    const handleDisplayTypeChange = (type) => {
+        setDisplayType(type);
+    }
+
     return (
         <div>
-            
-            <div>
-                {user_pins.map((pin) => (
-                    <div>
-                    <PinCard key={pin.id} pin={pin}/>
-                    </div>
-                ))}
+            <div className="profile-container">
+            <img className='profile-user-image' src={user.profile_img} alt={user.first_name} />
+            <h2>{user.first_name} {user.last_name}</h2>
+            <div className="profile-buttons">
+                <button
+                    className="profile-button"
+                    onClick={() => handleDisplayTypeChange("pin")}
+                >
+                    My Pins
+                </button>
+                <button
+                    className="profile-button"
+                    onClick={() => handleDisplayTypeChange("board")}
+                >
+                    My Boards
+                </button>
             </div>
-            <div>
-                {user_boards.map((board) => (
-                    <BoardCard key={board.id} board={board}/>
-                ))}
+            </div>
+            <div className="grid-container">
+                {displayType === "pin" && (
+                    user_pins.map((pin) => (
+                        <div className="grid-item" key={pin.id}>
+                            <PinCard pin={pin} />
+                        </div>
+                    ))
+                )}
+                {displayType === "board" && (
+                    user_boards.map((board) => (
+                        <div className="grid-item" key={board.id}>
+                            <BoardCard board={board} />
+                        </div>
+                    ))
+                )}
             </div>
         </div>
     )
