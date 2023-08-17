97f70d3cbd119841d4a12947b24ad411c34788eb
Alexandra <ale.vallem@gmail.com>
2023-08-17 17:25:35 -0400

    crate pin css completed

--- react-app/src/components/Pins/CreateSinglePin.js
+++ react-app/src/components/Pins/CreateSinglePin.js
@@ -1,43 +1,72 @@
-import React, { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { useHistory } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { useModal } from "../../context/Modal";
-import { createSinglePinThunk, getAllPinsThunk } from "../../store/pin"
-import './Pins.css'
+import { createSinglePinThunk, getAllPinsThunk } from "../../store/pin";
+import "./Pins.css";
 
 const CreateSinglePin = () => {
-
-    const dispatch = useDispatch()
-    const history = useHistory()
-    const { closeModal } = useModal()
-    const user = useSelector(state => state.session.user)
-    const [name, setName] = useState('')
-    const [description, setDescription] = useState('')
-    const [alt_text, setAlt_text] = useState('')
-    const [website, setWebsite] = useState('')
-    const [images, setImages] = useState(null)
-    const [errors, setErrors] = useState([])
-    const [frontendErrors, setFrontendErrors] = useState({})
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const { closeModal } = useModal();
+    const user = useSelector((state) => state.session.user);
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [alt_text, setAlt_text] = useState("");
+    const [website, setWebsite] = useState("");
+    const [images, setImages] = useState(null);
+    const [errors, setErrors] = useState([]);
+    const [frontendErrors, setFrontendErrors] = useState({});
+    const [submitted, setSubmitted] = useState(false)
     const [showInput, setShowInput] = useState(false);
-
+    const [dragging, setDragging] = useState(false);
+    const [uploadStatus, setUploadStatus] = useState("Drag and drop or");
+    const [imagePreview, setImagePreview] = useState(null);
 
     useEffect(() => {
-
-        const frontendErrors = {}
+        const frontendErrors = {};
 
         if (!images) {
-            frontendErrors.images = "An image is required to create a pin"
+            frontendErrors.images = "An image is required to create a pin";
         }
         if (!name) {
-            frontendErrors.name = "A title is required to post your pin"
+            frontendErrors.name = "A title is required to post your pin";
         }
         if (!description) {
-            frontendErrors.description = "A description is required to post your pin"
+            frontendErrors.description = "A description is required to post your pin";
         }
 
-        setFrontendErrors(frontendErrors)
+        setFrontendErrors(frontendErrors);
+    }, [images, name, description]);
 
-    }, [images, name, description])
+    const handleDrop = (e) => {
+        e.preventDefault();
+        setDragging(false);
+        const file = e.dataTransfer.files[0];
+        setImages(file);
+        setUploadStatus("Image selected and ready to upload");
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            setImagePreview(e.target.result);
+        };
+        reader.readAsDataURL(file);
+    };
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+        setDragging(true);
+    };
+
+    const handleFileSelect = (e) => {
+        const file = e.target.files[0];
+        setImages(file);
+        setUploadStatus("Image selected and ready to upload");
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            setImagePreview(e.target.result);
+        };
+        reader.readAsDataURL(file);
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -52,15 +81,15 @@ const CreateSinglePin = () => {
         formData.append("user_id", user.id);
 
         if (!images) {
-        	console.log("No image selected");
-        	return;
+            console.log("No image selected");
+            return;
         }
 
         await dispatch(createSinglePinThunk(formData));
         await dispatch(getAllPinsThunk());
-        await history.push('/user')
+        await history.push("/user");
         await closeModal();
-
+        
     };
 
     const toggleInput = () => {
@@ -68,63 +97,100 @@ const CreateSinglePin = () => {
     };
 
     return (
-        <div>
+        <div className="create-pin-container">
+            <h1>Create your Pin</h1>
             <form onSubmit={handleSubmit} encType="multipart/form-data">
-                <label>
-                    Image
-                    <input
-                        type="file"
-                        accept="image/*"
-                        onChange={(e) => setImages(e.target.files[0])}
-                    />
-                </label>
-                <div>
-                    <input
-                        type="text"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        placeholder="Add your title"
-                        required
-                    />
-                </div>
-                <div>
-                    <img src={user.profile_img} alt={user.first_name} style={{ width: '40px', height: '40px' }}></img>
-                    <h3>{user.first_name}</h3>
-                </div>
-                <div>
-                    <textarea
-                        type="text"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        placeholder="Tell everyone what your Pin is abaout"
-                        required
-                    />
-                </div>
-                <div>
-                    {showInput ? (
-                        <input
-                            type="text"
-                            value={alt_text}
-                            onChange={(e) => setAlt_text(e.target.value)}
-                            placeholder="Explain what people can see in this pin" />
-                    ) : (
-                        <button onClick={toggleInput}>Add alt text</button>
-                    )}
-                </div>
-                <div>
-                    <input
-                        type="text"
-                        value={website}
-                        onChange={(e) => setWebsite(e.target.value)}
-                        placeholder="Add a destination link "
-                    />
-                </div>
-                <div>
-                    <button type="submit">Save</button>
+                <div className="create-pin-img-form">
+                    <div className="create-pin-img-container">
+                        <div
+                            id="drop-area"
+                            onDrop={handleDrop}
+                            onDragOver={handleDragOver}
+                            className={dragging ? "dragging" : ""}
+                        >
+                            {imagePreview ? (
+                                <img src={imagePreview} alt="Image Preview" style={{ maxWidth: '100%', maxHeight: '200px' }} />
+                            ) : (
+                                uploadStatus
+                            )}
+                        </div>
+                        <div className="choose-file">
+
+                            {!imagePreview && (
+                                <input
+                                    type="file"
+                                    accept="image/*"
+                                    onChange={handleFileSelect}
+                                />
+                            )}
+                        </div>
+                        {frontendErrors.images && submitted && (
+                            <p className='error-message'>{frontendErrors.images}</p>
+                        )}
+                    </div>
+                    <div className="create-pin-form-details">
+                        <div className="create-pin-user-img-container">
+                            <img
+                                src={user.profile_img}
+                                alt={user.first_name}
+                                style={{ width: "50px", height: "50px" }}
+                            ></img>
+                            <h3>{user.first_name}</h3>
+                        </div>
+                        <div className="create-pin-form">
+                            <div>
+                                <input
+                                    type="text"
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
+                                    placeholder="Add your title"
+                                    required
+                                />
+                            </div>
+                            {frontendErrors.name && submitted && (
+                                <p className='error-message'>{frontendErrors.name}</p>
+                            )}
+                            <div>
+                                <textarea
+                                    type="text"
+                                    value={description}
+                                    onChange={(e) => setDescription(e.target.value)}
+                                    placeholder="Tell everyone what your Pin is about"
+                                    required
+                                />
+                            </div>
+                            {frontendErrors.description && submitted && (
+                                <p className='error-message'>{frontendErrors.description}</p>
+                            )}
+                            <div className="alt-text-button">
+                                {showInput ? (
+                                    <input
+                                        type="text"
+                                        value={alt_text}
+                                        onChange={(e) => setAlt_text(e.target.value)}
+                                        placeholder="Explain what people can see in this pin"
+                                    />
+                                ) : (
+                                    <button onClick={toggleInput}>Add alt text</button>
+                                )}
+                            </div>
+                            <div>
+                                <input
+                                    type="text"
+                                    value={website}
+                                    onChange={(e) => setWebsite(e.target.value)}
+                                    placeholder="Add a destination link"
+                                />
+                            </div>
+                        </div>
+                        <div className="create-pin-save-button">
+                            <button type="submit">Save</button>
+                        </div>
+                    </div>
                 </div>
             </form>
         </div>
-    )
-}
+    );
+};
 
-export default CreateSinglePin
+export default CreateSinglePin;
