894b9d00bf138c4a015e87c871b4be7321d6b887
Alexandra <ale.vallem@gmail.com>
2023-08-21 01:02:34 -0400

    added more pins seeds

--- app/seeds/pins.py
+++ app/seeds/pins.py
@@ -12,7 +12,7 @@ def seed_pins():
         images = 'https://i.pinimg.com/564x/37/81/48/378148ebb3451ced7ce554ad7fe949ae.jpg'
     )
     pin2 = Pin(
-        user_id = 1,
+        user_id = 2,
         name = 'Floating Village Adventure',
         description = 'Explore a charming village that sits atop tranquil waters, offering a glimpse into a unique way of life.',
         alt_text = 'Colorful houses on stilts over calm water.',
@@ -20,7 +20,7 @@ def seed_pins():
         images = 'https://i0.wp.com/www.myticklefeet.com/wp-content/uploads/2019/02/20180107_171430.jpg?ssl=1'
     )
     pin3 = Pin(
-        user_id = 1,
+        user_id = 3,
         name = "Cave Dwelles's Haven",
         description = 'Discover an otherworldly underground city carved into rock, filled with history and wonder.',
         alt_text = 'Intricate rock formations in an underground cave.',
@@ -36,7 +36,7 @@ def seed_pins():
         images = 'https://i.pinimg.com/564x/5a/ee/aa/5aeeaab5a41cc17e8f17dde3fe8d3663.jpg'
     )
     pin5 = Pin(
-        user_id = 1,
+        user_id = 2,
         name = 'Mystical Northern Lights',
         description = 'Witness the ethereal dance of the Northern Lights painting the night sky with vibrant colors.',
         alt_text = 'Aurora Borealis illuminating the dark sky.',
@@ -44,7 +44,7 @@ def seed_pins():
         images = 'https://i.pinimg.com/564x/7c/f1/e8/7cf1e8590bd3a07805b8e66d55ed481d.jpg'
     )
     pin6 = Pin(
-        user_id = 1,
+        user_id = 3,
         name = 'Suspended Mountain Monastery',
         description = 'Visit a monastery perched on a towering mountain, offering breathtaking views and spiritual solace.',
         alt_text = 'Ancient monastery clinging to the side of a mountain.',
@@ -60,7 +60,7 @@ def seed_pins():
         images = 'https://i.pinimg.com/564x/11/e1/21/11e121ebcd3131aba0c30ba969988200.jpg'
     )
     pin8 = Pin(
-        user_id = 1,
+        user_id = 2,
         name = 'Lost Ruins of the Jungle',
         description = 'Unearth the secrets of ancient ruins hidden deep within lush jungle foliage, a testament to history.',
         alt_text = 'Overgrown temple ruins in a dense jungle.',
@@ -68,7 +68,7 @@ def seed_pins():
         images = 'https://i.pinimg.com/564x/60/2f/8c/602f8c7406ae2dfcf157232b05cae0ae.jpg'
     )
     pin9 = Pin(
-        user_id = 1,
+        user_id = 3,
         name = 'Whimsical Hobbit Hamlet',
         description = 'Live out your fantasies in a whimsical village reminiscent of Hobbiton, nestled in a picturesque landscape.',
         alt_text = 'Quaint hobbit houses with colorful gardens.',
@@ -84,7 +84,7 @@ def seed_pins():
         images = 'https://i.pinimg.com/564x/8c/20/a9/8c20a9dc56744741455b38b2dd85643c.jpg'
     )
     pin11 = Pin(
-        user_id = 1,
+        user_id = 2,
         name = 'Cappadocia Hot Air Balloon Spectacle',
         description = 'Soar over Cappadocia, Turkey, in a hot air balloon, witnessing its iconic fairy chimney rock formations.',
         alt_text = 'Hot air balloons flying over Cappadocia unique landscape.',
@@ -100,7 +100,7 @@ def seed_pins():
         images = 'https://i.pinimg.com/564x/8b/8d/22/8b8d22ee3528479ef31e7823b2476730.jpg'
     )
     pin13 = Pin(
-        user_id = 1,
+        user_id = 2,
         name = 'Antelope Canyon Light Play',
         description = 'Marvel at the intricate light beams dancing through the narrow sandstone crevices of Antelope Canyon, USA.',
         alt_text = 'Sunlight creating stunning beams inside Antelope Canyon walls.',
@@ -108,7 +108,7 @@ def seed_pins():
         images = 'https://i.pinimg.com/564x/ec/44/7b/ec447be5d3e5754b6eb6f8507c3cb0f3.jpg'
     )
     pin14 = Pin(
-        user_id = 1,
+        user_id = 3,
         name = 'Great Barrier Reef Underwater Wonderland',
         description = 'Dive into the vibrant world of the Great Barrier Reef, Australia, exploring its stunning coral reefs and marine life.',
         alt_text = 'Colorful coral reef with various fish species underwater.',
@@ -132,7 +132,7 @@ def seed_pins():
         images = 'https://i.pinimg.com/564x/b0/24/c8/b024c8eb375b7872e7596d47c8fc3210.jpg'
     )
     pin17 = Pin(
-        user_id = 2,
+        user_id = 3,
         name = 'Aurora Borealis in Lapland',
         description = 'Witness the enchanting Northern Lights in Lapland, Finland, painting the night sky with vibrant colors.',
         alt_text = 'Aurora Borealis dancing across the night sky over snowy landscapes.',
@@ -140,7 +140,7 @@ def seed_pins():
         images = 'https://i.pinimg.com/564x/ae/16/20/ae1620b07c1825ab9f05e566abe8879f.jpg'
     )
     pin18 = Pin(
-        user_id = 2,
+        user_id = 1,
         name = 'Petra Rose-Red Beauty',
         description = 'Explore the awe-inspiring archaeological city of Petra, Jordan, famous for its carved rose-red rock structures.',
         alt_text = 'Elaborate rock-carved architecture in Petra, Jordan.',
@@ -156,54 +156,95 @@ def seed_pins():
         images = 'https://i.pinimg.com/564x/f4/42/a8/f442a83462c7a1a8f0453544c6a49496.jpg'
     )
     pin20 = Pin(
-        user_id = 2,
+        user_id = 3,
         name = 'Serengeti Great Migration',
         description = 'Witness the stunning spectacle of the Great Migration in the Serengeti, Tanzania, as wildebeests traverse the plains.',
         alt_text = 'Wildebeests on the move during the Great Migration in the Serengeti.',
         website = 'https://www.vacayweather.com/',
         images = 'https://i.pinimg.com/564x/c3/c2/13/c3c213e4249e0a07bfe0bc316c9013ae.jpg'
     )
-#         name = 'Santorini's Caldera Views',
-#         description = 'Indulge in the stunning vistas of Santorini's caldera, Greece, as you relax on the edge of volcanic cliffs.',
-#         alt_text = 'Aerial view of Santorini's caldera with blue domed buildings.'
+    pin21 = Pin(
+        user_id = 1,
+        name = "Santorini's Caldera Views",
+        description = "Indulge in the stunning vistas of Santorini's caldera, Greece, as you relax on the edge of volcanic cliffs.",
+        alt_text = "Aerial view of Santorini's caldera with blue domed buildings.",
+        website = 'https://www.itsallbee.com/2018/08/santorini-3-day-itinerary/',
+        images = 'https://i.pinimg.com/564x/ff/03/0d/ff030dec6ae6be52633ac5c8956be18f.jpg'
+    )
+    pin22 = Pin(
+        user_id = 2,
+        name = 'Japanese Cherry Blossom Delight',
+        description = "Immerse yourself in the beauty of Japan's cherry blossom season, when streets and parks are adorned with delicate pink blooms.",
+        alt_text = 'Cherry blossom trees in full bloom along a serene pathway.',
+        website = '',
+        images = 'https://i.pinimg.com/564x/6d/80/99/6d8099bddc65d50fde7f554df8673b3d.jpg'
+    )
+    pin23 = Pin(
+        user_id = 3,
+        name = "Eiffel Tower's City of Love",
+        description = 'Experience the romance of Paris, France, with the iconic Eiffel Tower casting its enchanting glow over the city.',
+        alt_text = 'Eiffel Tower illuminated against the night sky.',
+        website = 'https://wanderlustpulse.com/romantic-things-to-do-in-paris/',
+        images = 'https://i.pinimg.com/564x/60/f3/62/60f362a59467dababb4840a8e68514b7.jpg'
+    )
+    pin24 = Pin(
+        user_id = 1,
+        name = 'Majestic Machu Picchu Sunrise',
+        description = 'Catch the awe-inspiring sunrise over the ancient Incan city of Machu Picchu, Peru, for a truly magical experience.',
+        alt_text = 'Sunrise over Machu Picchu with misty mountains in the background.',
+        website = 'https://picchutravel.com/es/',
+        images = 'https://i.pinimg.com/564x/e2/a8/c2/e2a8c27c942305cb13d9e7ff38f04bda.jpg'
+    )
+    pin25 = Pin(
+        user_id = 2,
+        name = 'Awe-Inspiring Grand Canyon',
+        description = 'Stand at the edge of the Grand Canyon, USA, and witness the vastness and geological wonder of this natural masterpiece.',
+        alt_text = 'Panoramic view of the Grand Canyon with layered rock formations.',
+        website = 'https://fullsuitcase.com/grand-canyon-in-one-day/',
+        images = 'https://i.pinimg.com/564x/e1/2c/14/e12c146e9fb4892527801a7ee459524e.jpg'
+    )
+    pin26 = Pin(
+        user_id = 3,
+        name = 'Tropical Paradise in Maldives',
+        description = 'Relax in overwater bungalows and indulge in the crystal-clear waters and white sandy beaches of the Maldives.',
+        alt_text = 'Overwater bungalows surrounded by turquoise waters in the Maldives.',
+        website = 'https://www.ourworldstuff.com/2017/11/maldives-romantic-place-to-visit-any-time.html',
+        images = 'https://i.pinimg.com/564x/13/8e/2b/138e2b3393392def33d6f5ff2493c1d1.jpg'
+    )
+    pin27 = Pin(
+        user_id = 1,
+        name = 'Intriguing Stonehenge Enigma',
+        description = 'Contemplate the mysteries of Stonehenge, UK, as you stand before the enigmatic stone circle, a UNESCO World Heritage site.',
+        alt_text = 'Stonehenge with stones arranged in a circular pattern.',
+        website = 'https://www.pinterest.com/pin/48835977202676008/',
+        images = 'https://i.pinimg.com/564x/58/41/24/58412441e9242613ea8ed2fc99c1e448.jpg'
+    )
+    pin28 = Pin(
+        user_id = 2,
+        name = 'African Safari Adventure',
+        description = 'Embark on a thrilling safari through the plains of Africa, witnessing the incredible diversity of wildlife in their natural habitat.',
+        alt_text = 'Elephants and zebras in the African savannah during a safari.',
+        website = 'https://tinyurl.com/2w2hmsrv',
+        images = 'https://i.pinimg.com/564x/7a/4b/22/7a4b2201b8aa4a4b9471c23614314a04.jpg'
+    )
+    pin29 = Pin(
+        user_id = 3,
+        name = "Taj Mahal's Timeless Beauty",
+        description = 'Marvel at the exquisite architecture of the Taj Mahal, India, a testament to eternal love and artistic mastery.',
+        alt_text = 'Taj Mahal reflecting in the tranquil waters at sunrise.',
+        website = 'https://www.pinterest.com/pin/5348093301561985/',
+        images = 'https://i.pinimg.com/564x/e8/45/2b/e8452b4f2df623f32febb51cf8660e81.jpg'
+    )
+    pin30 = Pin(
+        user_id = 1,
+        name = 'Enchanting Norwegian Fjords',
+        description = 'Cruise through the mesmerizing fjords of Norway, surrounded by towering cliffs and stunning natural landscapes.',
+        alt_text = 'Majestic fjord surrounded by lush greenery and calm waters.',
+        website = 'https://norwaytravelguide.no/connect-with-locals/Vidar/the-geiranger-fjord',
+        images = 'https://i.pinimg.com/564x/17/31/71/17317109bd645fe818d72903987ec811.jpg'
+    )
 
-# name = 'Japanese Cherry Blossom Delight',
-# description = 'Immerse yourself in the beauty of Japan's cherry blossom season, when streets and parks are adorned with delicate pink blooms.',
-# alt_text = 'Cherry blossom trees in full bloom along a serene pathway.'
-
-# name = 'Eiffel Tower's City of Love',
-# description = 'Experience the romance of Paris, France, with the iconic Eiffel Tower casting its enchanting glow over the city.',
-# alt_text = 'Eiffel Tower illuminated against the night sky.'
-
-# name = 'Majestic Machu Picchu Sunrise',
-# description = 'Catch the awe-inspiring sunrise over the ancient Incan city of Machu Picchu, Peru, for a truly magical experience.',
-# alt_text = 'Sunrise over Machu Picchu with misty mountains in the background.'
-
-# name = 'Awe-Inspiring Grand Canyon',
-# description = 'Stand at the edge of the Grand Canyon, USA, and witness the vastness and geological wonder of this natural masterpiece.',
-# alt_text = 'Panoramic view of the Grand Canyon with layered rock formations.'
-
-# name = 'Tropical Paradise in Maldives',
-# description = 'Relax in overwater bungalows and indulge in the crystal-clear waters and white sandy beaches of the Maldives.',
-# alt_text = 'Overwater bungalows surrounded by turquoise waters in the Maldives.'
-
-# name = 'Intriguing Stonehenge Enigma',
-# description = 'Contemplate the mysteries of Stonehenge, UK, as you stand before the enigmatic stone circle, a UNESCO World Heritage site.',
-# alt_text = 'Stonehenge with stones arranged in a circular pattern.'
-
-# name = 'African Safari Adventure',
-# description = 'Embark on a thrilling safari through the plains of Africa, witnessing the incredible diversity of wildlife in their natural habitat.',
-# alt_text = 'Elephants and zebras in the African savannah during a safari.'
-
-# name = 'Taj Mahal's Timeless Beauty',
-# description = 'Marvel at the exquisite architecture of the Taj Mahal, India, a testament to eternal love and artistic mastery.',
-# alt_text = 'Taj Mahal reflecting in the tranquil waters at sunrise.'
-
-# name = 'Enchanting Norwegian Fjords',
-# description = 'Cruise through the mesmerizing fjords of Norway, surrounded by towering cliffs and stunning natural landscapes.',
-# alt_text = 'Majestic fjord surrounded by lush greenery and calm waters.'
-
-    pins_list = [pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8, pin9, pin10, pin11, pin12, pin13, pin14, pin15, pin16, pin17, pin18, pin19, pin20]
+    pins_list = [pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8, pin9, pin10, pin11, pin12, pin13, pin14, pin15, pin16, pin17, pin18, pin19, pin20, pin21, pin22, pin23, pin24, pin25, pin26, pin27, pin28, pin29, pin30]
     single_pin = [db.session.add(pin) for pin in pins_list]
     db.session.commit()
 
